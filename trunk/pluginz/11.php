d32f8c80c7586b766b974c9397797b6dfa4a6ee3
herfeb <herfeb@b9fe76b8-557e-11de-908a-311428b59617>
2010-01-11 12:09:08 +0000

    update megashares.com plugin by kaox 11.php

--- trunk/pluginz/11.php
+++ trunk/pluginz/11.php
@@ -1,20 +1,20 @@
 <?php
-if (!defined('RAPIDLEECH'))
-  {require_once("404.php");exit;}
 
-if (($_GET["premium_acc"] == "on" && $_GET["pr_user"] && $_GET["pr_pass"]) || ($_GET["premium_acc"] == "on" && $premium_acc["megashares"]["user"] && $premium_acc["megashares"]["pass"]))
+if (!defined('RAPIDLEECH'))
+  {
+  require_once("404.php");
+  exit;
+  }
+if (($_GET["premium_acc"] == "on" && $_GET["premium_user"] && $_GET["premium_pass"]) || ($_GET["premium_acc"] == "on" && $premium_acc["megashares"]["user"] && $premium_acc["megashares"]["pass"]))
 {
 	$post = Array();
-	$post["lc_email"] = (($_GET["pr_user"] && $_GET["pr_pass"]) ? $_GET["pr_user"] : $premium_acc["megashares"]["user"]);
-	$post["lc_pin"] = (($_GET["pr_user"] && $_GET["pr_pass"]) ? $_GET["pr_pass"] : $premium_acc["megashares"]["pass"]);
+	$post["lc_email"] = (($_GET["premium_user"] && $_GET["premium_pass"]) ? $_GET["premium_user"] : $premium_acc["megashares"]["user"]);
+	$post["lc_pin"] = (($_GET["premium_user"] && $_GET["premium_pass"]) ? $_GET["premium_pass"] : $premium_acc["megashares"]["pass"]);
 	$post["lc_signin"] = "Sign-In";
 	$Url = parse_url("http://www.megashares.com/");
 	$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), 0, 0, $post, 0, $_GET["proxy"], $pauth);
 	is_page($page);
-	//is_present($page, "Failed login message:", "Wrong username or password");
-	
-	is_present_tryagain($page, "Failed login message:", "Wrong username or password");
-	
+	is_present($page, "Failed login message:", "Wrong username or password");
 	is_notpresent($page, "Set-Cookie: linkcard=", "Cannot use premium account");
 	preg_match_all("/Set-Cookie: ([^\;]+)/", $page, $matches);
 	$cookies = implode("; ", $matches[1]);
@@ -30,7 +30,7 @@ if (($_GET["premium_acc"] == "on" && $_GET["pr_user"] && $_GET["pr_pass"]) || ($
 	{
 		$Url = parse_url($href[1]);
 		$FileName = basename($href[1]);
-		insert_location("$PHP_SELF?filename=".urlencode($FileName)."&host=".$Url["host"]."&path=".urlencode($Url["path"].($Url["query"] ? "?".$Url["query"] : ""))."&referer=".urlencode($Referer)."&cookie=".urlencode($cookies)."&email=".($_GET["domail"] ? $_GET["email"] : "")."&partSize=".($_GET["split"] ? $_GET["partSize"] : "")."&cookie=".urlencode($cookies)."&proxy=".($_GET["useproxy"] ? $_GET["proxy"] : "")."&saveto=".$_GET["path"]."&link=".urlencode($LINK).($_GET["add_comment"] == "on" ? "&comment=".urlencode($_GET["comment"]) : "")."&auth=".$auth.($pauth ? "&pauth=$pauth" : "").(isset($_GET["idx"]) ? "&idx=".$_GET["idx"] : ""));
+		insert_location("$PHP_SELF?filename=".urlencode($FileName)."&host=".$Url["host"]."&path=".urlencode($Url["path"].($Url["query"] ? "?".$Url["query"] : ""))."&referer=".urlencode($Referer)."&cookie=".urlencode($cookies)."&email=".($_GET["domail"] ? $_GET["email"] : "")."&partSize=".($_GET["split"] ? $_GET["partSize"] : "")."&cookie=".urlencode($cookies)."&proxy=".($_GET["useproxy"] ? $_GET["proxy"] : "")."&saveto=".$_GET["path"]."&link=".urlencode($LINK).($_GET["add_comment"] == "on" ? "&comment=".urlencode($_GET["comment"]) : "")."&auth=".$auth.($pauth ? "&pauth=$pauth" : "").(isset($_GET["audl"]) ? "&audl=doum" : ""));
 	}
 	else
 	{
@@ -47,7 +47,7 @@ else
 		$randnum = $_POST["randnum"];
 		$pass = $_POST["pass"];
 		$mhost = $_POST["mhost"];
-		$dllink = "http://".$_POST["dllink"];
+		$dllink = $_POST["dllink"];
 		$rndtime = $_POST["rndtime"];
 		$name = $_POST["name"];
 		
@@ -60,15 +60,14 @@ else
 			$Url = parse_url($dllink);
 			$FileName = $name;
 			
-			insert_location("$PHP_SELF?filename=".urlencode($FileName)."&host=".$Url["host"]."&path=".urlencode($Url["path"].($Url["query"] ? "?".$Url["query"] : ""))."&referer=".urlencode($Referer)."&email=".($_GET["domail"] ? $_GET["email"] : "")."&partSize=".($_GET["split"] ? $_GET["partSize"] : "")."&method=".$_GET["method"]."&cookie=".urlencode($cookies)."&proxy=".($_GET["useproxy"] ? $_GET["proxy"] : "")."&saveto=".$_GET["path"]."&link=".urlencode($LINK).($_GET["add_comment"] == "on" ? "&comment=".urlencode($_GET["comment"]) : "")."&auth=".$auth.($pauth ? "&pauth=$pauth" : "").(isset($_GET["idx"]) ? "&idx=".$_GET["idx"] : ""));
+			insert_location("$PHP_SELF?filename=".urlencode($FileName)."&host=".$Url["host"]."&path=".urlencode($Url["path"].($Url["query"] ? "?".$Url["query"] : ""))."&referer=".urlencode($Referer)."&email=".($_GET["domail"] ? $_GET["email"] : "")."&partSize=".($_GET["split"] ? $_GET["partSize"] : "")."&method=".$_GET["method"]."&cookie=".urlencode($cookies)."&proxy=".($_GET["useproxy"] ? $_GET["proxy"] : "")."&saveto=".$_GET["path"]."&link=".urlencode($LINK).($_GET["add_comment"] == "on" ? "&comment=".urlencode($_GET["comment"]) : "")."&auth=".$auth.($pauth ? "&pauth=$pauth" : "").(isset($_GET["audl"]) ? "&audl=doum" : ""));
 		}else{
 			html_error("An error occured", 0);
 		}
 	}else{
 		$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "?".$Url["query"] : ""), $Referer, 0, 0, 0, $_GET["proxy"],$pauth);
 		is_page($page);
-		preg_match_all('/Set-Cookie: *(.+);/', $page, $cook);
-		$cookies = implode(';', $cook[1]);
+		$cookies = GetCookies($page);
 		
 		$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].($Url["query"] ? "index.php?".$Url["query"] : ""), $Referer, $cookies, 0, 0, $_GET["proxy"],$pauth);
 		is_page($page);
@@ -79,17 +78,16 @@ else
 		is_present($page, "Link was removed", "File not found");
 		is_present($page,"This link's <u>filesize is larger</u> than what you have left on your Passport.");
 		is_present($page, "You can download again when your download completes", "This IP address is already downloading a file");
-		
-		preg_match_all('/Set-Cookie: *(.+);/', $page, $cook);
-		$cookies = implode(';', $cook[1]);
-		
-		preg_match('%id="dlink".*http://(.*?)"%', $page, $dllink);
-		preg_match('%Filename:&nbsp;<strong>(.+?)</%', $page, $name);
+		$cookies .= "; ".GetCookies($page);		
+		preg_match('%http://.+megashares\.com/index\.php\?d01=[^"\']+%', $page, $dllink);
+		$name = cut_str ( $dllink[0] ,'fln=/' ,'"' );
 		
 		if(preg_match('/Your Passport needs to be reactivated/', $page)){
 			preg_match('/var request_uri *= *"(.*)";/', $page, $fquery);
 			preg_match('/src="index\.php\?secgfx=gfx&random_num=(.+?)"/', $page, $randnum);
 			preg_match('/name="passport_num".*value="(.*?)"/', $page, $pass);
+            is_present($page,"All download slots for this link are currently filled", "The server response is: All download slots for this link are currently filled.
+Please try again momentarily.", 0);
 			
 			$img = "http://".$Url["host"]."/index.php?secgfx=gfx&random_num=".$randnum[1];
 			$mhost = $Url["host"];
@@ -100,7 +98,7 @@ else
 			$pass_img = substr($page,$headerend+4);
 			write_file($download_dir."megashare_captcha.png", $pass_img);
 			
-		print 	"<form method=\"post\" action=\"".$PHP_SELF.(isset($_GET["idx"]) ? "?idx=".$_GET["idx"] : "")."\" name=\"msform\">$nn";
+			print 	"<form method=\"post\" action=\"".$PHP_SELF.(isset($_GET["audl"]) ? "?audl=doum" : "")."\" name=\"msform\">$nn";
 			print	"<b>Enter the passport reactivation code in the graphic, then hit the 'Reactivate Passport' button.</b><br><br>$nn";
 			print	"<img src=\"{$download_dir}megashare_captcha.png?id=".rand(10000, 100000)."\" >$nn";
 			print	"<input name=\"link\" value=\"$LINK\" type=\"hidden\">$nn";
@@ -109,20 +107,23 @@ else
 			print	"<input name=\"randnum\" value=\"$randnum[1]\" type=\"hidden\">$nn";
 			print	"<input name=\"pass\" value=\"$pass[1]\" type=\"hidden\">$nn";
 			print	"<input name=\"cookies\" value=\"$cookies\" type=\"hidden\">$nn";
-			print	"<input name=\"dllink\" value=\"$dllink[1]\" type=\"hidden\">$nn";
-			print	"<input name=\"name\" value=\"$name[1]\" type=\"hidden\">$nn";
+			print	"<input name=\"dllink\" value=\"$dllink[0]\" type=\"hidden\">$nn";
+			print	"<input name=\"name\" value=\"$name\" type=\"hidden\">$nn";
 			print	"<input name=\"captcha\" type=\"text\" >$nn";
 			print "<input type=\"hidden\" name=\"rndtime\">\n";
 			print	"<input name=\"ms\" value=\"ok\" type=\"hidden\">$nn";
 			print "<script language=\"JavaScript\">function check() { document.msform.rndtime.value=new Date().getTime(); return true; }</script>$nn";
 			print	"<input name=\"Submit\" onclick=\"return check()\" value=\"Reactivate Passport\" type=\"submit\"></form>";
 		}else{
-			$dllink = "http://".$dllink[1];
-			$Url = parse_url($dllink);
-			$FileName = $name[1];
+			$dwn = $dllink[0];
+			$Url = parse_url($dwn);
+			$FileName = $name;
 					
-			insert_location("$PHP_SELF?filename=".urlencode($FileName)."&host=".$Url["host"]."&path=".urlencode($Url["path"].($Url["query"] ? "?".$Url["query"] : ""))."&referer=".urlencode($Referer)."&email=".($_GET["domail"] ? $_GET["email"] : "")."&partSize=".($_GET["split"] ? $_GET["partSize"] : "")."&cookie=".urlencode($cookies)."&proxy=".($_GET["useproxy"] ? $_GET["proxy"] : "")."&saveto=".$_GET["path"]."&link=".urlencode($LINK).($_GET["add_comment"] == "on" ? "&comment=".urlencode($_GET["comment"]) : "")."&auth=".$auth.($pauth ? "&pauth=$pauth" : "").(isset($_GET["idx"]) ? "&idx=".$_GET["idx"] : ""));
+			insert_location("$PHP_SELF?filename=".urlencode($FileName)."&host=".$Url["host"]."&path=".urlencode($Url["path"].($Url["query"] ? "?".$Url["query"] : ""))."&referer=".urlencode($Referer)."&email=".($_GET["domail"] ? $_GET["email"] : "")."&partSize=".($_GET["split"] ? $_GET["partSize"] : "")."&cookie=".urlencode($cookies)."&proxy=".($_GET["useproxy"] ? $_GET["proxy"] : "")."&saveto=".$_GET["path"]."&link=".urlencode($LINK).($_GET["add_comment"] == "on" ? "&comment=".urlencode($_GET["comment"]) : "")."&auth=".$auth.($pauth ? "&pauth=$pauth" : "").(isset($_GET["audl"]) ? "&audl=doum" : ""));
 		}
 	}
 }
+
+// update by kaox 09-jan-2010 (FIX for free download)
+
 ?>
