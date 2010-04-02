7676fb2452cd442c2dc6ab09f33cd30bb04b94cd
1d03nk <1d03nk@b9fe76b8-557e-11de-908a-311428b59617>
2010-04-02 11:55:09 +0000

    Update ifile.it plugin

--- trunk/pluginz/40.php
+++ trunk/pluginz/40.php
@@ -1,116 +1,200 @@
 <?php 
 if (!defined('RAPIDLEECH'))
   {require_once("404.php");exit;}
+  
+  
+	Download( $LINK );
+	function Download( $link )
+	{
+	   /*
+	    global $premium_acc, $hf_cookie_auth_value;		
+		if( ($_REQUEST ["premium_acc"] == "on" && $_REQUEST ["premium_user"] && $_REQUEST ["premium_pass"]) ||
+			($_REQUEST ["premium_acc"] == "on" && $premium_acc ["ifile_it"] ["user"] && $premium_acc ["ifile_it"] ["pass"])			
+		  )
+		{
+			DownloadPremium($link);
+		}
+		else
+		*/
+		{
+			DownloadFree($link);
+		}
+	}
 
-
-if($_POST["ffi"] == "ok"){
- $cookie=$_POST["cookie"];
-$ttemp= $_POST["link"]  ;
-$ddlreq= str_replace("type=na&esn=1","type=simple&esn=1&".$_POST["vimg"]."=".$_POST["captcha_check"],$ttemp);
-$Url = parse_url($ddlreq);
-$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].'?'.$Url['query'], $loc, $cookie, 0, 0, $_GET["proxy"],$pauth);
-is_present($page, "show_captcha", $strerror = "The captha inserted is wrong", 0) ;
-$Url = parse_url("http://ifile.it/dl");
-$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"], $loc, $cookie, 0, 0, $_GET["proxy"],$pauth);
-
-$snap = cut_str ( $page ,'id="req_btn2' ,'>download' );
-$Href  = cut_str ( $snap ,'href="' ,'"' );
-
-$Url = parse_url($Href);
-$FileName = !$FileName ? basename($Url["path"]) : $FileName;
-
-insert_location("$PHP_SELF?cookie=".urlencode($cook)."&filename=".urlencode($FileName)."&host=".$Url["host"]."&path=".urlencode($Url["path"].($Url["query"] ? "?".$Url["query"] : ""))."&referer=".urlencode($Referer)."&email=".($_GET["domail"] ? $_GET["email"] : "")."&partSize=".($_GET["split"] ? $_GET["partSize"] : "")."&method=".$_GET["method"]."&proxy=".($_GET["useproxy"] ? $_GET["proxy"] : "")."&saveto=".$_GET["path"]."&link=".urlencode($LINK).($_GET["add_comment"] == "on" ? "&comment=".urlencode($_GET["comment"]) : "")."&auth=".$auth.($pauth ? "&pauth=$pauth" : "").(isset($_GET["idx"]) ? "&idx=".$_GET["idx"] : ""));
-
-}else{
-$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"], $Referer, 0, 0, 0, $_GET["proxy"],$pauth);
-is_page($page);
-preg_match_all('/Set-Cookie: (.*);/U',$page,$temp);
-$cook2 = $temp[1];
-$cookie = implode(';',$cook2)."; ".$cook;
-$Referer = $LINK;
-preg_match('/ocation: (.*)/',$page,$loc);
-$loc = trim($loc[1]);
-$Url = parse_url($loc);
-$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"], 0, $cookie, 0, 0, $_GET["proxy"],$pauth);
-$__x_fsa = cut_str ( $page ,"var __x_fsa = '" ,"'" );
-$__x_fs = cut_str ( $page ,"var __x_fs = '" ,"'" );
-$__x_fs2 = cut_str ( $page ,'__x_fs + "' ,'"' );
-$__x_c = cut_str ( $page ,"var __x_c = '" ,"'" );
-/*
-preg_match('/<input.*name="file_key".*value="(.*)"/',$page,$file_key);
-$file_key = $file_key[1];
-$dllink = "http://ifile.it/download:dl_request?it=".$file_key.",type=na,esn=1";
-*/
-$dllink = "http://ifile.it/download:dl_request?".$__x_fsa."&type=na&esn=1&".$__x_fs.$__x_fs2  ;
-
-$Url = parse_url($dllink);
-$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].'?'.$Url['query'], $loc, $cookie, 0, 0, $_GET["proxy"],$pauth);
-is_page($page);
-
-if (strpos($page,'captcha":"none'))
-{
-    /*
-$Url = parse_url($dllink);
-$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].'?'.$Url['query'], $loc, $cook, 0, 0, $_GET["proxy"],$pauth);
-*/
-$Url = parse_url($loc);
-$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"], $loc, $cookie, 0, 0, $_GET["proxy"],$pauth);
-$Referer = $loc;
-/*
-preg_match('/<a name="req_btn".*href="(.*)"/', $page, $loc);
-$Href = $loc[1];
-*/
-
-$snap = cut_str ( $page ,'id="req_btn2' ,'>download' );
-$Href  = cut_str ( $snap ,'href="' ,'"' );
-
-$Url = parse_url($Href);
-$FileName = !$FileName ? basename($Url["path"]) : $FileName;
-
-insert_location("$PHP_SELF?cookie=".urlencode($cook)."&filename=".urlencode($FileName)."&host=".$Url["host"]."&path=".urlencode($Url["path"].($Url["query"] ? "?".$Url["query"] : ""))."&referer=".urlencode($Referer)."&email=".($_GET["domail"] ? $_GET["email"] : "")."&partSize=".($_GET["split"] ? $_GET["partSize"] : "")."&method=".$_GET["method"]."&proxy=".($_GET["useproxy"] ? $_GET["proxy"] : "")."&saveto=".$_GET["path"]."&link=".urlencode($LINK).($_GET["add_comment"] == "on" ? "&comment=".urlencode($_GET["comment"]) : "")."&auth=".$auth.($pauth ? "&pauth=$pauth" : "").(isset($_GET["idx"]) ? "&idx=".$_GET["idx"] : ""));
-
-} elseif (strpos($page,'captcha":"simple')){
-
-$rnd='0.'.idnum(16);
-$access_image_url='http://ifile.it/download:captcha?'.$rnd;
-$Url = parse_url($access_image_url);
-$page = geturl($Url["host"], $Url["port"] ? $Url["port"] : 80, $Url["path"].'?'.$Url['query'], "http://ifile.it/dl", $cookie, 0, 0, $_GET["proxy"],$pauth);
-is_page($page);
+	function DownloadFree( $link )
+	{
+	  global $download_dir, $PHP_SELF, $nn;
 	
-       
-        $headerend = strpos($page,"JFIF");
-        $pass_img = substr($page,$headerend-6);
-        $imgfile=$download_dir."ifile_captcha.jpg"; 
-        if (file_exists($imgfile)){ unlink($imgfile);} 
-		write_file($imgfile, $pass_img);
-	
+	  $Referer = $link;
+	  $dl_link = "http://ifile.it/dl";
+      if($_POST["ffi"] == "ok"){
+        $cookie=$_POST["cookie"];
+        $ttemp= $_POST["link"]  ;
+        $ddlreq= str_replace("type=na&esn=1","type=simple&esn=1&".$_POST["vimg"]."=".$_POST["captcha_check"],$ttemp);
 		
-	print 	"<form method=\"post\" action=\"".$PHP_SELF.(isset($_GET["idx"]) ? "?idx=".$_GET["idx"] : "")."\">$nn";
-	print	"<b>Please enter code:</b><br>$nn";
-	print	"<img src=\"$imgfile\">$nn";
-	print	"<input name=\"cookie\" value=\"$cookie\" type=\"hidden\">$nn";
-	print	"<input name=\"ffi\" value=\"ok\" type=\"hidden\">$nn";
-	print	"<input name=\"link\" value=\"$dllink\" type=\"hidden\">$nn";
-	print	"<input name=\"vimg\" value=\"$__x_c\" type=\"hidden\">$nn";	
-	print	"<input name=\"captcha_check\" type=\"text\" >";
-	print	"<input name=\"Submit\" value=\"Submit\" type=\"submit\"></form>";
+        $page = GetPage( $ddlreq, $cookie, 0, $Referer );
+        is_present($page, "show_captcha", $strerror = "The captha inserted is wrong", 0) ;
+        
+		$page = GetPage( $dl_link, $cookie, 0, $Referer );        
+        $snap = cut_str ( $page ,'id="req_btn' ,'>download' );
+        $Href  = cut_str ( $snap ,'href="' ,'"' );
+        
+        $FileName = basename($Href);
+        
+		RedirectDownload( $Href, $FileName );
+		exit ();
+        
+      }else{
 
-    
-    
-}else{
-html_error( "If you have already reattempted more times then this plugin not work , probably changes have been made on the page. Report it in the Rapidleech forum" , 0 );
-}
-}
+        $page = GetPage( $link );
+		$cookie = GetCookies( $page );
+		
+        if(preg_match('/ocation: (.*)/',$page,$loc)){
+           $loc = trim($loc[1]);
+		   $Referer = $loc;
+           $page = GetPage( $loc, $cookie, 0, $Referer);
+		
+		   if(preg_match("/var\s__alias_id.+=(?:[\t]+)*\'(.+)'/", $page, $cocok)){
+		     $alias_id = $cocok[1];
+			 $dllink = "http://ifile.it/download:dl_request?alias_id=".$alias_id."&type=na&esn=1";
+		   }
+		   
+		   $page = GetPage( $dllink, $cookie, 0, $Referer);
+		}else{
+		   html_error( "Error get link ifile.it/dl, probably changes have been made on the page." , 0 );
+		}
+        
+		
+        //if (strpos($page,'captcha":"none'))
+        if (strpos($page,'captcha":0') )
+        {
+		   $page = GetPage( $loc, $cookie, 0, $Referer);           
+           $snap = cut_str ( $page ,'id="req_btn' ,'>download' );
+           $Href  = cut_str ( $snap ,'href="' ,'"' );
+           
+		   $FileName = basename($Href);        
+		   RedirectDownload( $Href, $FileName );
+		   exit ();
 
-function idnum ($ll){
-$id = "";
-for($i=0; $i<$ll; $i++)
-$id .= floor(rand(0, 9));
-return $id;
-}
+	   } elseif (strpos($page,'captcha":"simple')){
+        
+           $rnd='0.'.idnum(16);
+		   $Referer = "http://ifile.it/dl";
+           $access_image_url='http://ifile.it/download:captcha?'.$rnd;
+		   $page = GetPage( $access_image_url, $cookie, 0, $Referer);
+               
+           $headerend = strpos($page,"JFIF");
+           $pass_img = substr($page,$headerend-6);
+           $imgfile=$download_dir."ifile_captcha.jpg"; 
+           if (file_exists($imgfile)){ unlink($imgfile);} 
+           write_file($imgfile, $pass_img);        	
+
+           print "<form method=\"post\" action=\"".$PHP_SELF.(isset($_GET["idx"]) ? "?idx=".$_GET["idx"] : "")."\">$nn";
+           print "<b>Please enter code:</b><br>$nn";
+           print "<img src=\"$imgfile\">$nn";
+           print "<input name=\"cookie\" value=\"$cookie\" type=\"hidden\">$nn";
+           print "<input name=\"ffi\" value=\"ok\" type=\"hidden\">$nn";
+           print "<input name=\"link\" value=\"$dllink\" type=\"hidden\">$nn";
+           print "<input name=\"vimg\" value=\"$__x_c\" type=\"hidden\">$nn";	
+           print "<input name=\"captcha_check\" type=\"text\" >";
+           print "<input name=\"Submit\" value=\"Submit\" type=\"submit\"></form>";       
+
+        }else{
+           html_error( "If you have already reattempted more times then this plugin not work , probably changes have been made on the page. Report it in the Rapidleech forum" , 0 );
+        }
+      }
+	
+	}  // end DownloadFree
 
 
+	function DownloadPremium($link)
+	{
+	   // not implemented yet =====================
+	}
+	
+  	/**
+	 * You can use this function to retrieve pages without parsing the link
+	 * 
+	 * @param string $link The link of the page to retrieve
+	 * @param string $cookie The cookie value if you need
+	 * @param array $post name=>value of the post data
+	 * @param string $referer The referer of the page, it might be the value you are missing if you can't get plugin to work
+	 * @param string $auth Page authentication, unneeded in most circumstances
+	 */
+	function GetPage($link, $cookie = 0, $post = 0, $referer = 0, $auth = 0) {
+		global $pauth;
+		if (!$referer) {
+			global $Referer;
+			$referer = $Referer;
+		}
+		$Url = parse_url(trim($link));
+		$page = geturl ( $Url ["host"], $Url ["port"] ? $Url ["port"] : 80, $Url ["path"] . ($Url ["query"] ? "?" . $Url ["query"] : ""), $referer, $cookie, $post, 0, $_GET ["proxy"], $pauth, $auth );
+		is_page ( $page );
+		return $page;
+	}
+	
+	/**
+	 * Use this function instead of insert_location so that we can improve this feature in the future
+	 * 
+	 * @param string $link The download link of the file
+	 * @param string $FileName The name of the file
+	 * @param string $cookie The cookie value
+	 * @param array $post The post value will be serialized here
+	 * @param string $referer The page that refered to this link
+	 * @param string $auth In format username:password
+	 * @param array $params This parameter allows you to add extra _GET values to be passed on
+	 */
+	function RedirectDownload($link, $FileName, $cookie = 0, $post = 0, $referer = 0, $auth = "", $params = array()) {
+		global $pauth;
+		if (!$referer) {
+			global $Referer;
+			$referer = $Referer;
+		}
+		$Url = parse_url($link);
+		
+		if (substr($auth,0,6) != "&auth=") $auth = "&auth=" . $auth;
+		if (!is_array($params)) {
+			// Some problems with the plugin, quit it
+			html_error('Plugin problem! Please report, error: "The parameter passed must be an array"');
+		}
+		$addon = "";
+		if (count((array) $params) > 0) {
+			foreach ($params as $name => $value) {
+				if (is_array($value)) {
+					$value = serialize($value);
+				}
+				$addon .= '&'.$name.'='.urlencode($value).'&';
+			}
+			$addon = substr($addon,0,-1);
+		}
+		$loc = "{$_SERVER['PHP_SELF']}?filename=" . urlencode ( $FileName ) . 
+			"&host=" . $Url ["host"] . "&port=" . $Url ["port"] . "&path=" . 
+			urlencode ( $Url ["path"] . ($Url ["query"] ? "?" . $Url ["query"] : "") ) . 
+			"&referer=" . urlencode ( $referer ) . "&email=" . ($_GET ["domail"] ? $_GET ["email"] : "") . 
+			"&partSize=" . ($_GET ["split"] ? $_GET ["partSize"] : "") . "&method=" . $_GET ["method"] . 
+			"&proxy=" . ($_GET ["useproxy"] ? $_GET ["proxy"] : "") . "&saveto=" . $_GET ["path"] . 
+			"&link=" . urlencode ( $link ) . ($_GET ["add_comment"] == "on" ? "&comment=" . 
+			urlencode ( $_GET ["comment"] ) : "") . $auth . ($pauth ? "&pauth=$pauth" : "") . 
+			($_GET ["uploadlater"] ? "&uploadlater=".$_GET["uploadlater"]."&uploadtohost=".$_GET['uploadtohost'] : "") .
+			"&cookie=" . urlencode($cookie) .
+			"&post=" . urlencode ( serialize ( $post ) ) .
+			($_POST ["uploadlater"] ? "&uploadlater=".$_POST["uploadlater"]."&uploadtohost=".urlencode($_POST['uploadtohost']) : "").
+			($_POST ['autoclose'] ? "&autoclose=1" : "").
+			(isset($_GET["idx"]) ? "&idx=".$_GET["idx"] : "") . $addon;
+
+		insert_location ( $loc );
+	}
+
+    function idnum ($ll){
+      $id = "";
+      for($i=0; $i<$ll; $i++)
+         $id .= floor(rand(0, 9));
+      return $id;
+    }
+
+/************************ifile.it**********************************
 // written by kaox 24-may-2009
 // update by kaox 15-nov-2009
-
+// update by Idx 01-apr-2010 - +Rebuild with Malhotra's OOP code
+************************ifile.it*********************************/
 ?>
