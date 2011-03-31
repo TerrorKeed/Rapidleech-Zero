6ab74422eef46f432a7d726ae9a25a5a449aed8f
ruudvtony@gmail.com <ruudvtony@gmail.com@b9fe76b8-557e-11de-908a-311428b59617>
2011-03-31 15:07:49 +0000

    [TEMP] Add function for upload plugin [UPDATE] Youtube Download plugin now support captcha

--- trunk/classes/http.php
+++ trunk/classes/http.php
@@ -787,10 +787,49 @@ if($errno || $errstr)
 
 
 echo "File <b>".$filename."</b>, size <b>".bytesToKbOrMb($fileSize)."</b>...<br>";
+?>
+<table cellspacing=0 cellpadding=0 style="FONT-FAMILY: Tahoma; FONT-SIZE: 11px;" id=progressblock>
+<tr>
+	<td width=100>&nbsp;</td>
+	<td width=300 nowrap>
+		<div style="border:#BBBBBB 1px solid; width:300px; height:10px;" class="progressborder">
+    		<div id="progress" style="background-color:#18f20d; margin:1px; width:0%; height:8px;"></div>
+		</div>
+	</td>
+<td width=100>&nbsp;</td>
+<tr>
+	<td align=right id=received width=100 nowrap>0 KB</td>
+	<td align=center id=percent width=300>0%</td>
+	<td align=left id=speed width=100 nowrap>0 KB/s</td>
+</tr>
+</table>
+<script>
+function pr(percent, received, speed)
+{
+	document.getElementById("received").innerHTML = '<b>' + received + '</b>';
+	document.getElementById("percent").innerHTML = '<b>' + percent + '%</b>';
+	document.title= '['+percent + '%]->['+orlink+']'+' Uploaded';
+	if (percent > 90) {percent=percent-1;}
+	document.getElementById("progress").style.width = percent + '%';
+	document.getElementById("speed").innerHTML = '<b>' + speed + ' KB/s</b>';
+	return true;
+}
 
-global $id;
-$id = md5 ( time () * rand ( 0, 10 ) );
-require_once(CLASS_DIR.'uploadui.php');
+
+
+
+function mail(str, field)
+{
+	document.getElementById("mailPart." + field).innerHTML = str;
+	return true;
+}
+</script>
+<br>
+<?php
+
+//global $id;
+//$id = md5 ( time () * rand ( 0, 10 ) );
+//require_once(CLASS_DIR.'uploadui.php');
 flush();
 
 $timeStart=getmicrotime();
